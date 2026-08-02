e1b63b8149be68afc766d730a33cf2fa64819ac1
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-08-02 11:07:38 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from "express";
+import errorHandler from "./middleware/errorHandler.js";
+import notFound from "./middleware/notFound.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/", (_req, res) => {
   res.status(200).json({
-    message: "Api is running",
+    success: true,
+    message: "API is running",
   });
 });
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 export default app;
---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+
+const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  console.error(err);
+
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+};
+
+export default errorHandler;
