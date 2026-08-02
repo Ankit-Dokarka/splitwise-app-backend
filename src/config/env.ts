93ddac96b699d88ef7a8db0b7c36b084f725b68e
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-08-02 11:35:07 +0530

    add real google auth

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { PORT, MONGODB_URI } = process.env;
+const { PORT, MONGODB_URI, GOOGLE_CLIENT_ID } = process.env;
 
 if (!PORT) {
   throw new Error("Missing required environment variable: PORT");
@@ -12,7 +12,12 @@ if (!MONGODB_URI) {
   throw new Error("Missing required environment variable: MONGODB_URI");
 }
 
+if (!GOOGLE_CLIENT_ID) {
+  throw new Error("Missing required environment variable: GOOGLE_CLIENT_ID");
+}
+
 export const env = {
   PORT: Number(PORT),
   MONGODB_URI,
+  GOOGLE_CLIENT_ID,
 } as const;
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,20 @@
-export const googleAuth = async (body: unknown) => {
+import googleClient from "../config/google.js";
+
+export const googleAuth = async (body: { idToken: string }) => {
+  const { idToken } = body;
+
+  const ticket = await googleClient.verifyIdToken({
+    idToken,
+  });
+
+  const payload = ticket.getPayload();
+
+  if (!payload) {
+    throw new Error("Unable to verify Google token.");
+  }
+
   return {
     success: true,
-    message: "Google Auth endpoint working",
-    data: body,
+    data: payload,
   };
 };
